6272fed3931fd3bae8ecbeb58dfe2f7e2c3ce9d7
yeonjae <yeonjae978@gmail.com>
2023-05-11 15:57:01 +0900

    2023-05-11 네번째 커밋

--- src/App.js
+++ src/App.js
@@ -1,31 +1,68 @@
-import React, { useState } from 'react';
-import axios from '../node_modules/axios/index';
+// import React, { useState } from 'react';
+// import axios from '../node_modules/axios/index';
+
+// const App = () => {
+//   const [data, setData] = useState(null);
+//   const onClick = async () => {
+//     // axios.get("https://jsonplaceholder.typicode.com/todos/1")
+//     // .then(response => {
+//     //   setData(response.data)
+//     // });
+//     try{
+
+//       const response = await axios.get(
+//         "https://newsapi.org/v2/top-headlines?country=kr&apiKey=95dd9e7aee234b2595c98eb9a7fa59c5"
+//       )
+//       setData(response.data);
+//     }catch(e){
+//       console.log(e);
+//     }
+//   };
+//   return (
+//     <div>
+//       <div>
+//         <button onClick={onClick}>불러오기</button>
+//       </div>
+//       {data && <textarea rows={7} value={JSON.stringify(data, null, 2)} readOnly={true} />} 
+//     </div>
+//   );
+// };
+
+// export default App;
+
+
+
+import React, { useCallback, useState } from 'react';
+import NewsList from './components/NewsList';
+import Categories from './components/Categories';
 
 const App = () => {
-  const [data, setData] = useState(null);
-  const onClick = async () => {
-    // axios.get("https://jsonplaceholder.typicode.com/todos/1")
-    // .then(response => {
-    //   setData(response.data)
-    // });
-    try{
+  const [category, setCategory] = useState('all');
+  const onSelect = useCallback(category => setCategory(category), []);
 
-      const response = await axios.get(
-        "https://newsapi.org/v2/top-headlines?country=kr&apiKey=95dd9e7aee234b2595c98eb9a7fa59c5"
-      )
-      setData(response.data);
-    }catch(e){
-      console.log(e);
-    }
-  };
   return (
-    <div>
-      <div>
-        <button onClick={onClick}>불러오기</button>
-      </div>
-      {data && <textarea rows={7} value={JSON.stringify(data, null, 2)} readOnly={true} />} 
-    </div>
-  );
+  <>
+    <Categories category={category} onSelect={onSelect} />
+    <NewsList category={category}/>
+  </>
+  )
 };
 
 export default App;
+
+
+
+// import React from 'react';
+// import { Route, Routes } from 'react-router-dom';
+// import NewsPage from './pages/NewsPage';
+
+// const App = () => {
+//   return (
+//     <Routes>
+//       <Route path="/" element={<NewsPage />} />
+//       <Route path="/:category" element={<NewsPage />} />
+//     </Routes>
+//   );
+// };
+
+// export default App;
---
+++ src/components/NewsList.js
@@ -0,0 +1,59 @@
+import React, { useEffect, useState } from 'react';
+import NewsItem from './NewsItem';
+import styled from 'styled-components';
+import axios from 'axios';
+
+const NewsListBlock = styled.div`
+  box-sizing: border-box;
+  padding-bottom: 3rem;
+  width: 768px;
+  margin: 2rem auto 0;
+  @media screen and (max-width: 768px){
+    width: 100%;
+    padding-left: 1rem;
+    padding-right: 1rem;
+  }
+`;
+
+// const sampleArticle = {
+//   title: '제목',
+//   description: '내용',
+//   url: 'https://google.com',
+//   urlToImage: 'https://via.placeholder.com/160',
+// }
+
+const NewsList = ({category}) => {
+  const [articles, setArticles] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+      const query = category === 'all' ? '' : `&category=${category}`;
+      const response = await axios.get(
+        `https://newsapi.org/v2/top-headlines?country=us${query}&apiKey=95dd9e7aee234b2595c98eb9a7fa59c5`,
+      );
+      setArticles(response.data.articles);
+      setLoading(false);
+    };
+    fetchData();
+  },[category]);
+
+  if(loading){
+    return <NewsListBlock>대기 중...</NewsListBlock>;
+  }
+  if(!articles){
+    return null;
+  }
+  return (
+    <NewsListBlock>
+      
+
+      {articles.map(article => (
+        <NewsItem key={article.url} article={article} />
+      ))}
+    </NewsListBlock>
+  );
+};
+
+export default NewsList;
