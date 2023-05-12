782757032da23ebdb9ff05307aa187e533752036
yeonjae <yeonjae978@gmail.com>
2023-05-12 10:18:12 +0900

    2023-05-12 async/await Promise 관련 라이브러리 추가 및 컴포넌트 수정

--- src/components/NewsList.js
+++ src/components/NewsList.js
@@ -1,7 +1,8 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import NewsItem from './NewsItem';
 import styled from 'styled-components';
 import axios from 'axios';
+import usePromise from '../lib/usePromise';
 
 
 const NewsListBlock = styled.div`
@@ -16,41 +17,47 @@ const NewsListBlock = styled.div`
   }
 `;
 
-// const sampleArticle = {
-//   title: '제목',
-//   description: '내용',
-//   url: 'https://google.com',
-//   urlToImage: 'https://via.placeholder.com/160',
-// }
-
 const NewsList = ({category}) => {
-  const [articles, setArticles] = useState(null);
-  const [loading, setLoading] = useState(false);
-
-  useEffect(() => {
-    try{
-      setLoading(true);
-      const fetchData = async () => {
+  // const [articles, setArticles] = useState(null);
+  // const [loading, setLoading] = useState(false);
+  const [loading, response, error] = usePromise(() => {
         const query = category === 'all' ? '' : `&category=${category}`;
-        const response = await axios.get(
-          // `https://newsapi.org/v2/top-headlines?country=kr${query}&apiKey=95dd9e7aee234b2595c98eb9a7fa59c5`,
+        return  axios.get(
           `https://newsapi.org/v2/top-headlines?country=us${query}&apiKey=3e3041e3f41848b5a0a96b6f28776a3d`
         );
-        setArticles(response.data.articles);
-        setLoading(false); // false를 안하면 데이터는 계속 로딩됨 => 429 Error 유발 (일정 시간이 지나야 풀림)
-      };
-      fetchData();
-    }catch(e){
-      console.log(e);
-    }
-  },[category]);
+  }, [category]);
+
+  // useEffect(() => {
+  //   try{
+  //     setLoading(true);
+  //     const fetchData = async () => {
+  //       const query = category === 'all' ? '' : `&category=${category}`;
+  //       const response = await axios.get(
+  //         // `https://newsapi.org/v2/top-headlines?country=kr${query}&apiKey=95dd9e7aee234b2595c98eb9a7fa59c5`,
+  //         `https://newsapi.org/v2/top-headlines?country=us${query}&apiKey=3e3041e3f41848b5a0a96b6f28776a3d`
+  //       );
+  //       setArticles(response.data.articles);
+  //       setLoading(false); // false를 안하면 데이터는 계속 로딩됨 => 429 Error 유발 (일정 시간이 지나야 풀림)
+  //     };
+  //     fetchData();
+  //   }catch(e){
+  //     console.log(e);
+  //   }
+  // },[category]);
+
+
 
   if(loading){
     return <NewsListBlock>대기 중...</NewsListBlock>;
   }
-  if(!articles){
+  if(!response){
     return null;
   }
+  if(error){
+    return <NewsListBlock>에러 발생!</NewsListBlock>;
+  }
+
+  const { articles } = response.data;
   return (
     <NewsListBlock>
       {articles.map(article => (
